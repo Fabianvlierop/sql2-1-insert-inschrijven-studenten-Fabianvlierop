6c6806064dc243ba451200f1ed16cdd55ee61be9
Fabianvlierop <45451428+Fabianvlierop@users.noreply.github.com>
2019-05-10 12:35:54 +0200

    Studenten aangemaakt

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,7 @@
 USE `lo8e_sql2`;
 
-INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
-VALUES ('lo9e-amo1', 'rs001', 2019, 'amo'),
-('lo9e-amo2', 'brc01', 2019, 'amo'),
-('lo9e-amo3', 'bkr02', 2019, 'amo');
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) 
+VALUES (90123,'Rutte','','Mark','1800-04-23','1122 PW','Schiphol', 81.2, 1.86, '2018-01-01'),
+(90234,'Wilders','','Geert','1700-01-13','1234 QW','Haarlem', 92.2, 1.96, '2018-01-01'),
+(90345,'Rutte','','Mark','1600-02-24','1232 PS','Amsterdam', 75.2, 1.76, '2018-01-01'),
+(90456,'Rutte','','Mark','1500-07-12','1432 PG','Rotterdam', 78.2, 1.64, '2018-01-01');
