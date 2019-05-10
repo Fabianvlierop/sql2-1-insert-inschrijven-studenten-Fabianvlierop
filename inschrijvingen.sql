b8cde10973962080139aba32261b909bb4d648d4
Fabianvlierop <45451428+Fabianvlierop@users.noreply.github.com>
2019-05-10 12:40:18 +0200

    Koppelen student aan klas

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,7 @@
 USE `lo8e_sql2`;
 
-INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) 
-VALUES (90123,'Rutte','','Mark','1800-04-23','1122 PW','Schiphol', 81.2, 1.86, '2018-01-01'),
-(90234,'Wilders','','Geert','1700-01-13','1234 QW','Haarlem', 92.2, 1.96, '2018-01-01'),
-(90345,'Rutte','','Mark','1600-02-24','1232 PS','Amsterdam', 75.2, 1.76, '2018-01-01'),
-(90456,'Rutte','','Mark','1500-07-12','1432 PG','Rotterdam', 78.2, 1.64, '2018-01-01');
+INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES 
+(90123,'lo8e-amo1'),
+(90234,'lo8e-amo2'),
+(90345,'lo8e-amo3'),
+(90456,'lo8e-amo4');
