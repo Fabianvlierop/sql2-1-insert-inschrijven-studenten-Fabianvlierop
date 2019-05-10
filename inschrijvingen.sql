445c6fee0502bd731372be1d500b2326a67bffb4
Fabianvlierop <45451428+Fabianvlierop@users.noreply.github.com>
2019-05-10 12:48:08 +0200

    Wijziging student

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,7 +1,5 @@
 USE `lo8e_sql2`;
 
-INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES 
-(90123,'lo8e-amo1'),
-(90234,'lo8e-amo2'),
-(90345,'lo8e-amo3'),
-(90456,'lo8e-amo4');
+UPDATE 	studenten 
+SET geboortedatum = 1550-07-12
+WHERE ov_nummer = 90456;
