4e9c3fdfb8ab1db45e97d1a8182f555f19107068
Fabianvlierop <45451428+Fabianvlierop@users.noreply.github.com>
2019-05-13 23:00:00 +0200

    Overzicht inschrijving

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,10 @@
 USE `lo8e_sql2`;
 
-UPDATE studenten 
-SET actief = 0
-WHERE inschrijvings_datum <= '2017-01-01';
+SELECT studenten.actief, studenten.ov_nummer, CONCAT(studenten.voornaam, ' ', studenten.tussenvoegsel, ' ', studenten.achternaam) AS volledige_naam, 
+klassen.klas_code, klassen.cohort, CONCAT(docenten.voorletters, ' ', docenten.tussenvoegsel, ' ', docenten.achternaam) AS slb_naam 
+FROM studenten
+LEFT JOIN klassen_studenten ON klassen_studenten.ov_nummer = studenten.ov_nummer
+LEFT JOIN klassen ON klassen.klas_code = klassen_studenten.klas_code
+LEFT JOIN docenten ON docenten.docent_code = klassen.slb_code
+WHERE studenten.actief = 1
+ORDER BY klassen.klas_code, studenten.achternaam ASC;
